762364b3207332d9271ba7970e93f1fc520ef59a
Donschki <donvonkrog@gmail.com>
2022-09-02 18:01:28 +0200

    a

--- Contact Form/contactFormHandler.php
+++ Contact Form/contactFormHandler.php
@@ -22,7 +22,7 @@ $email_body = "User Name: $name.\n".
 
                   $headers .= "Reply-To: $visitor_email \r\n"
 
-                  mail($to, $email_subject, $email_body, $headers);
+                  mail($to,$email_subject,$email_body,$headers);
 
                   header("Location: index.html");
 
